482a11e33a44410a979ed429815aa69170556d57
danielleelara <dany.sa1@hotmail.com>
2022-06-04 09:34:50 -0300

    feat: incluindo buscador, variaveis e header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,13 +1,25 @@
+import React, {useState} from 'react';
 import styles from './Cardapio.module.scss';
-import logo from 'assets/logo.svg';
+import {ReactComponent as Logo} from 'assets/logo.svg';
+import Buscador from './Buscador';
 
 export default function Cardapio() {
+  const [busca, setBusca] = useState('')
+  
   return (
     <main>
       <nav className={styles.menu}>
-        <img src={logo} alt="Logo" />
-        <p>Aluroni</p>
+        <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do Código e da Massa
+        </div>
+        <section className={styles.cardapio}>
+          <h3  className={styles.cardapio__titulo}>Cardápio</h3>
+          <Buscador busca={busca} setBusca={setBusca}/>
+        </section>
+      </header>
     </main>
   )
 }
