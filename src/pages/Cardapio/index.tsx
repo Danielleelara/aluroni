d88099f912e4aa4e83c33576d60cbebbc9560f6a
danielleelara <dany.sa1@hotmail.com>
2022-06-04 18:11:14 -0300

    feat: criando filtros

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,11 @@ import React, {useState} from 'react';
 import styles from './Cardapio.module.scss';
 import {ReactComponent as Logo} from 'assets/logo.svg';
 import Buscador from './Buscador';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('')
+  const [filtro, setFiltro] = useState<number | null>(null);
   
   return (
     <main>
@@ -15,11 +17,14 @@ export default function Cardapio() {
         <div className={styles.header__text}>
           A casa do Código e da Massa
         </div>
+      </header>
         <section className={styles.cardapio}>
           <h3  className={styles.cardapio__titulo}>Cardápio</h3>
           <Buscador busca={busca} setBusca={setBusca}/>
+          <div  className={styles.cardapio__filtros}>
+            <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          </div>
         </section>
-      </header>
     </main>
   )
 }
