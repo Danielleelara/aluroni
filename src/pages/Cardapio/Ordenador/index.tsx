7568e54d64edae896788b11c04519edce6029d59
danielleelara <dany.sa1@hotmail.com>
2022-06-05 20:24:47 -0300

    feat: incluindo icones e estilos

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Ordenador.module.scss';
-import opcoes from './opcoes.json'
+import opcoes from './opcoes.json';
+import classNames from 'classnames';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
 export default function Ordenador() {
+  const [open, setOpen] = useState(false);
+
+  console.log(open)
+
   return (
-    <button className={styles.ordenador}>
+    <button 
+      onBlur={()=> setOpen(false)}
+      onClick={()=> setOpen(!open)}
+      className={styles.ordenador}>
       <span>Ordenar Por</span>
-      <div className={styles.ordenador__options}>
+      {open ? < MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+      <div 
+        className={classNames({
+        [styles.ordenador__options]: true,
+        [styles["ordenador__options--ativo"]]: open,
+
+      })}>
         {opcoes.map((opcao) =>
           <div
             className={styles.ordenador__option}
