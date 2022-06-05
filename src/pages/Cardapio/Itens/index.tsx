d7221078aba7069b5b7f29f438317a4a2ae1e0c8
danielleelara <dany.sa1@hotmail.com>
2022-06-05 20:51:44 -0300

    feat: criando lista de itens

---
+++ src/pages/Cardapio/Itens/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import cardapio from './itens.json';
+
+export default function Itens() {
+  return (
+    <div>
+      {cardapio.map((item)=>(
+        <div>
+          {item.title}
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -4,26 +4,34 @@ import opcoes from './opcoes.json';
 import classNames from 'classnames';
 import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
 
-export default function Ordenador() {
-  const [open, setOpen] = useState(false);
+interface Props {
+  ordenador: string,
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>,
+}
 
-  console.log(open)
+export default function Ordenador({ordenador, setOrdenador}: Props) {
+  const [open, setOpen] = useState(false);
+  const nomeOrdenador = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome;
 
   return (
     <button 
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles["ordenador--ativo"]]: ordenador !== '',
+      })}
       onBlur={()=> setOpen(false)}
       onClick={()=> setOpen(!open)}
-      className={styles.ordenador}>
-      <span>Ordenar Por</span>
+      >
+    <span>{nomeOrdenador || "Ordenar Por"}</span>
       {open ? < MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
       <div 
         className={classNames({
         [styles.ordenador__options]: true,
         [styles["ordenador__options--ativo"]]: open,
-
       })}>
         {opcoes.map((opcao) =>
           <div
+            onClick={()=> setOrdenador(opcao.value)}
             className={styles.ordenador__option}
             key={opcao.value}
           >
